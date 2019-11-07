b743aae1057017cf0734d7e921742c73efc88475
ItsJustMeChris <christian.mcgravey@gmail.com>
2019-11-06 20:46:55 -0500

    Fix
    Fix typo with relation to job.

--- models/Company.js
+++ models/Company.js
@@ -5,5 +5,5 @@ module.exports = async function (fastify) {
     name: Sequelize.STRING
   });
   fastify.db.models.Company.hasMany(fastify.db.models.Job, { as: 'job' });
-  fastify.db.models.Job.belongsTo(fastify.db.models.User);
+  fastify.db.models.Job.belongsTo(fastify.db.models.Company);
 }
