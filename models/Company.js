2406d5b1c25e0acf2e9279a772e6d85b921d9711
ItsJustMeChris <christian.mcgravey@gmail.com>
2019-11-08 02:15:27 -0500

    Update
    Updated company model

--- models/Company.js
+++ models/Company.js
@@ -2,7 +2,9 @@ const Sequelize = require('sequelize');
 
 module.exports = async function (fastify) {
   fastify.db.define('Company', {
-    name: Sequelize.STRING
+    name: Sequelize.STRING,
+    description: Sequelize.STRING,
+    image: Sequelize.STRING
   });
   fastify.db.models.Company.hasMany(fastify.db.models.Job, { as: 'job' });
   fastify.db.models.Job.belongsTo(fastify.db.models.Company);
--- routes/Company.js
+++ routes/Company.js
@@ -1,5 +1,7 @@
 module.exports = async function (fastify, opts) {
   const { Company, SessionToken, User } = fastify.db.models;
+  const sanitizeHtml = require('sanitize-html');
+
   /*
     @URL /{version}/company/new
     @METHOD POST
@@ -10,13 +12,18 @@ module.exports = async function (fastify, opts) {
   */
   fastify.post('/new', async (req, res) => {
     res.type('application/json').code(200);
-    const { token, name } = req.body;
+    const { token, name, description, image } = req.body;
+    const cleanDescription = sanitizeHtml(description, {
+      allowedTags: ['h2', 'h1', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
+        'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
+        'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'iframe', 'img'],
+    });
     const session = await SessionToken.findOne({ where: { token } });
     if (!session) return { status: 'error', message: 'Invalid Session Token' };
     const user = await User.findOne({ where: { id: session.UserId } });
     if (!user) return { status: 'error', message: 'Failed to find User' };
     const transaction = await fastify.db.transaction();
-    const company = await Company.create({ name });
+    const company = await Company.create({ name, description: cleanDescription, image });
     if (!company) return { status: 'error', message: 'Failed to create company' };
     await transaction.commit();
     user.addCompany(company);
